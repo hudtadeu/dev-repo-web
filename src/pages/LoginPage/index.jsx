e727cd6174457685842449c910223893ffd3587f
Hudson Tadeu <hudinvesting@gmail.com>
2022-12-30 11:51:32 -0300

    criação de parâmetro de busca

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-const LoginPage = () => (
-  <h1>Login Page</h1>
-);
+import "./styles.cs"
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('email', email);
+    console.log('password', password);
+    console.log('login');
+  }
+
+  return (
+  <div id="login">
+    <div className="title">login</div>
+    <div className="form">
+      <div className="field">
+        <label htmlFor="email">Email:</label>
+        <input 
+          type="email" 
+          name="email" 
+          id="email" 
+          value={email}
+          onChange={(e) => setEmail(e.target.value) } />
+      </div>
+      
+      <div className="field">
+        <label htmlFor="password">Senha:</label>
+        <input 
+          type="password" 
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value) } />
+      </div>
+
+      <div className="actions">
+        <button onClick={handleLogin}>Entrar</button>
+      </div>
+    </div>
+  </div>
+  )
+};
 
 export default LoginPage;
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
-const MainPage = () => (
-  <h1>Main Page</h1>
-)
+import "./styles.css";
+
+const MainPage = () => {
+  const handleLogout = () => {
+    console.log('logout');
+  }
+
+  const handleSearch = (query) => {
+    console.log('query', query);
+  }
+
+  const handleClear = () => {
+    console.log('clear');
+  }
+
+  return (
+    <div id="main">
+      <div className="nav">
+        <h1 className="logo">SisRepo</h1>
+        <button onClick={handleLogout}>Sair</button>
+      </div>
+
+      <div className="search">
+        <label htmlFor="query">Procurar:</label>
+        <input type="search" name="query" id="query" />
+        <button onClick={handleClear}>Limpar</button>
+        <button onClick={handleSearch}>Procurar</button>
+      </div>
+    </div>
+  )
+}
 
 export default MainPage;
