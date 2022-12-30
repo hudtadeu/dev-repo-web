e21eda45693b5c6794713ba80a74c2546db49a89
Hudson Tadeu <hudinvesting@gmail.com>
2022-12-30 16:15:23 -0300

    Separando componentes

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,68 +1,39 @@
-import React from "react";
+import React from 'react';
 
-import "./styles.css";
+import Nav from './Nav';
+import Search from './Search';
+import Repositories from './Repositories';
+
+import './styles.css';
 
 const MainPage = () => {
   const handleLogout = () => {
     console.log('logout');
-  }
+  };
 
   const handleSearch = (query) => {
     console.log('query', query);
-  }
+  };
 
-  const handleClear = () => {
-    console.log('clear');
-  }
+  const handleDeleteRepo = (repository) => {
+    console.log('delete repo', repository);
+  };
 
-  const handleDeleteRepo = () => {
-    console.log('delete repo');
+  const handleNewRepo = (url) => {
+    console.log('new repo', url);
   }
 
   return (
     <div id="main">
-      <div className="nav">
-        <h1 className="logo">SisRepo</h1>
-        <button onClick={handleLogout}>Sair</button>
-      </div>
-
-      <div className="search">
-        <label htmlFor="query">Procurar:</label>
-        <input type="search" name="query" id="query" />
-        <button onClick={handleClear}>Limpar</button>
-        <button onClick={handleSearch}>Procurar</button>
-      </div>
-
-      <div className="repositories">
-        <h2 className="title">Repositórios</h2>
-
-        <ul className="list">
-          <li className="item">
-            <div className="info">
-              <div className="owner">facebook</div>
-              <div className="name">react</div>
-            </div>
-            <button>Apagar</button>
-          </li>
-
-          <li className="item">
-            <div className="info">
-              <div className="owner">facebook</div>
-              <div className="name">react-native</div>
-            </div>
-            <button onClick={handleDeleteRepo}>Apagar</button>
-          </li>
-
-        </ul>
-
-        <div className="new">
-          <label htmlFor="new-repo">Novo Repo:</label>
-          <input type="url" name="new-repo" id="new-repo" />
-          <button>Adicionar</button>
-        </div>
-      </div>
+      <Nav onLogout={handleLogout} />
+      <Search onSearch={handleSearch} />
+      <Repositories 
+      repositores={[]} 
+      onDeleteRepo={handleDeleteRepo} 
+      onNewRepo={handleNewRepo}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default MainPage;
